bf2831b037cf9e68718423e3ed4638a9c158ac42
Maximiliano Arias <max@max-arias.com>
2020-03-28 13:32:15 -0300

    Cleaning up, adding links, etc

--- src/components/timeline/list.js
+++ src/components/timeline/list.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import { gemBaseUrl } from '../../utils/constants.js'
 
+//TODO: emotion css
+
 export default ({ data }) => {
     return (
         <VerticalTimeline>
@@ -17,7 +19,7 @@ export default ({ data }) => {
                             border: '1px solid',
                         }}
                         iconStyle={{
-                            backgroundColor: '0c0b0b',
+                            backgroundColor: '#141414',
                             color: '#141414',
                             display: 'flex',
                             justifyContent: 'center',
@@ -29,32 +31,40 @@ export default ({ data }) => {
                     >
                         <div>
                             {data[lvl].map(gem => {
-                                const gemUrl = `/lib/images/${gem.skillId}.jpg`.replace('Support', 'Support/').replace(' ', '')
                                 return (
-                                    <div>
-                                        <div>
-                                            <img src={gemUrl} alt={gem.skillId} width="50" />
-                                            <h4>{gem.name}</h4>
+                                    <div className="gem-container" style={{ padding: '.5rem', marginBottom: '.5rem', backgroundColor: '#141414' }}>
+                                        <div className="gem-title" style={{ display: 'flex', marginBottom: '.5rem', alignItems: 'center', borderColor: '#141414' }}>
+                                            <img src={`/lib/images/${gem.skillId}.png`} alt={gem.skillId} width="50" />
+                                            <h4>
+                                                <a target="_blank" rel="noopener" href={`https://pathofexile.gamepedia.com/${gem.name}`} style={{ textDecoration: 'underline' }}>{gem.name}</a>
+                                            </h4>
                                         </div>
-                                        <hr />
+                                        <hr style={{ borderColor: '#A38D6D', marginBottom: '.5rem' }} />
+
+                                        {!gem.questReward && !gem.vendorReward && (
+                                            <span>This gem is not awarded to your class, you'll need to buy it.</span>
+                                        )}
+
                                         {gem.questReward && gem.questReward.length && (
-                                            <div>
+                                            <div className="gem-quest-reward">
+                                                <span>Quest Reward</span>
                                                 {gem.questReward.map(q => (
                                                     <div>
                                                         <span>Act: {q.act}</span>
-                                                        <span> - <a target="_blank" rel="noopener" href={`https://pathofexile.gamepedia.com/${q.questName}`}>{q.questName}</a></span>
+                                                        <span> - <a target="_blank" rel="noopener" href={`https://pathofexile.gamepedia.com/${q.questName}`} style={{ textDecoration: 'underline' }}>{q.questName}</a></span>
                                                     </div>
                                                 ))}
                                             </div>
                                         )}
+
                                         {gem.vendorReward && gem.vendorReward.length && (
-                                            <div>
+                                            <div className="gem-vendor-reward" style={{ marginTop: 12 }}>
+                                                <span>Vendor Reward</span>
                                                 {gem.vendorReward.map(v => (
                                                     <div>
                                                         <span>Act: {v.act}</span>
-                                                        <span> - <a target="_blank" rel="noopener" href={`https://pathofexile.gamepedia.com/${v.questName}`}>{v.questName}</a></span>
-                                                        <br />
-                                                        <span>Vendor: {v.vendorName}</span>
+                                                        {v.questName && (<span> - <a target="_blank" rel="noopener" href={`https://pathofexile.gamepedia.com/${v.questName}`} style={{ textDecoration: 'underline' }}>{v.questName}</a></span>)}
+                                                        <span> - {v.vendorName}</span>
                                                     </div>
                                                 ))}
                                             </div>
